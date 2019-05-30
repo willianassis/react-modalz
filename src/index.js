7a456d9f95085087038570c438635c2da03bca29
Willian Assis <willian.assis@ltm.digital>
2019-05-30 11:15:03 -0300

    removing comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
-// Export all the explicitly exported components, this file will contain our
-// components when built by webpack and sent off to the world.
 import { Modalz, ModalzReducers, ModalzActions } from './Modalz';
-
 export { Modalz, ModalzReducers, ModalzActions };
